68330823206c074cbd425fe74a179f94ca068ec0
Marlin <Marlin_Phone@outlook.com>
2025-10-05 22:26:11 +0800

    docs: 更新README.md内容并添加新的示例文件
    - 修改了README.md的标题为 "WebLab"
    - 更新了README.md中的所有 "Template-Repository" 为 "WebLab"
    - 替换了README.md中的wakatime徽章链接和图片链接
    - 更新了README.md中的其他徽章链接，包括最后提交、周提交活动、访问者、顶级语言和许可证
    - 添加了新的README.md内容：`This is a repository for the MIT web.lab course 2025 project.`
    - 添加了新的HTML文件 `hello.html`，包含基本的HTML结构和一些示例元素
    - 添加了新的CSS文件 `style.css`，定义了 `.info` 类和 `div` 的样式
    - 添加了新的JavaScript文件 `test.js`，其中包含一个 `printSomething` 函数和一些 `setTimeout` 示例，但存在逻辑错误
    
    test.js中的`setTimeout`调用中有两个错误：
    - `setTimeout(printSomething(), 2000);` 应为 `setTimeout(printSomething, 2000);`，直接传递函数而不执行
    - `setTimeout(console.log("i don't sleep"), 2000);` 应为 `setTimeout(() => console.log("i don't sleep"), 2000);`，使用箭头函数来延迟执行 `console.log`

---
+++ test.js
@@ -0,0 +1,43 @@
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = arr1;
+// let copyArr = [...arr1];
+
+// if(arr1 === arr2){
+//     console.log("Both arrays are the same");
+// }else{
+//     console.log("Both arrays are different");
+// }
+
+// if(arr1 === copyArr){
+//     console.log("Both arrays are the same");
+// }else{
+//     console.log("Both arrays are different");
+// }
+
+// console.log(arr1);
+// console.log(arr2);
+// console.log(copyArr);
+// console.log();
+// arr1[4] = 9;
+// copyArr[4] = 6;
+// console.log(arr1);
+// console.log(arr2);
+// console.log(copyArr);
+
+// const celsiusToFahrenheit = (tempCelsius) => {
+//     const tempFahrenheit = (tempCelsius * 9/5) + 32;
+//     return tempFahrenheit;
+// }
+
+// console.log(celsiusToFahrenheit(25)); // Output: 77
+
+
+const printSomething = () => {
+    console.log("i sleep");
+}
+// printSomething(); // Output: i sleep
+
+setTimeout(printSomething, 2000);
+setTimeout(printSomething(), 2000);
+setTimeout(console.log("i don't sleep"), 2000);
